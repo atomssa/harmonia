35ec0b9a3b00dcada0760bbd1fd48e0f5d00463c
Ermias <etatomssa@gmail.com>
2025-01-01 19:00:41 +0100

    more css retouch

--- src/app/components/Intervals.jsx
+++ src/app/components/Intervals.jsx
@@ -11,18 +11,18 @@ import { quals } from "../utils/consts";
 export default function Intervals({ root, qual }) {
   return (
     <div className="flex flex-col lg:flex-row text-primary ml-4 p-1 border-primary border-2 rounded-lg w-fit">
-      <div className="ml-2 lg:ml-5">
+      <div className="ml-2 lg:ml-5 mb-1 lg:mb-0">
         <span className="mr-1">Chord:</span>
         <span className="rnd">
           {formatRoot(root)}
           <sub>{mapFindByValue(quals, (x) => x.il === qual).k}</sub>
         </span>
       </div>
-      <div className="ml-2 lg:ml-5">
+      <div className="ml-2 lg:ml-5 mb-1 lg:mb-0">
         <span className="mr-1">Intervals:</span>
         <span className="mr-1">{pprint(intervals(root, qual))}</span>
       </div>
-      <div className="ml-2 lg:ml-5">
+      <div className="ml-2 lg:ml-5 mb-1 lg:mb-0">
         <span className="mr-1">Notes:</span>
         <span className="mr-1">{pprint(notes(root, qual))}</span>
       </div>
