ff9f10fa895d7a318431485b0305204495905594
Ermias Atomssa <ermias.atomssa@alizetrading.com>
2025-01-02 18:12:10 +0100

    begin wrok on caged form specification

--- src/app/utils/consts.jsx
+++ src/app/utils/consts.jsx
@@ -24,24 +24,209 @@ export const finger_sty = {
 
 export const ren_all = { P1: "R" };
 
+export const caged_all = {
+  C: [1, 3, 5, 1, 3],
+  A: [1, 5, 1, 3, 5],
+  G: [1, 3, 5, 1, 5, 1],
+  E: [1, 5, 1, 3, 5, 1],
+  D: [1, 5, 1, 3],
+};
+
+const fix = {
+  C: { 2: 7, 4: 0 },
+};
+
+export const fix_caged = (fix) => {
+  var ret = { ...caged_all };
+  Object.keys(fix).forEach((k) => {
+    if (k in fix) {
+      ret[k] = caged_all[k]
+        .map((x, i) => {
+          return i in fix[k] ? fix[k][i] : x;
+        })
+        .filter((x) => x !== 0);
+    } else {
+      ret[k] = caged_all[k];
+    }
+  });
+  return ret;
+};
+
 // il: interval list
 // prune: to remove
 // ren: rename intervals (eg R instead of P1)
 export const quals = new Map([
-  ["M", { il: "", prune: [], ren: { ...ren_all } }],
-  ["m", { il: "m", prune: [], ren: { ...ren_all } }],
-  ["7", { il: "Mm7", prune: [], ren: { ...ren_all } }],
-  ["m7", { il: "mm7", prune: [], ren: { ...ren_all } }],
-  ["Δ", { il: "MM7", prune: [], ren: { ...ren_all } }],
-  ["ø", { il: "mb5m7", prune: [], ren: { ...ren_all } }],
-  ["o", { il: "mb5M6", prune: [], ren: { ...ren_all, M6: "♭♭7" } }],
-  ["m6", { il: "m3m6", prune: [], ren: { ...ren_all } }],
-  ["7(9)", { il: "Mm7m9", prune: ["P5"], ren: { ...ren_all } }],
-  ["7(♭9)", { il: "Mm7b9", prune: ["P5"], ren: { ...ren_all } }],
-  ["7(#9)", { il: "Mm7#9", prune: ["P5"], ren: { ...ren_all, A9: "#9" } }],
-  ["7(♭13)", { il: "Mb13", prune: ["M9", "P11", "P5"], ren: { ...ren_all } }],
-  ["7(13)", { il: "Mm7m13", prune: ["M9", "P11", "P5"], ren: { ...ren_all } }],
-  ["m7(9)", { il: "mm7m9", prune: ["P5"], ren: { ...ren_all } }],
-  ["m7(11)", { il: "mm7m11", prune: ["P5", "M9"], ren: { ...ren_all } }],
-  ["Δ(13)", { il: "MM7m13", prune: ["P5", "P11", "M9"], ren: { ...ren_all } }],
+  [
+    "M",
+    {
+      il: "",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "m",
+    {
+      il: "m",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7",
+    {
+      il: "Mm7",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: fix_caged({
+        C: { 2: 7, 5: 0 },
+        E: { 2: 7 },
+        D: { 2: 7 },
+        A: { 2: 7 },
+      }),
+    },
+  ],
+  [
+    "m7",
+    {
+      il: "mm7",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: fix_caged({
+        E: { 2: 7 },
+        A: { 2: 7 },
+        D: { 2: 7 },
+      }),
+    },
+  ],
+  [
+    "Δ",
+    {
+      il: "MM7",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: fix_caged({
+        E: { 1: 0, 2: 7, 5: 0 },
+        A: { 2: 7, 4: 0 },
+        D: { 2: 7 },
+      }),
+    },
+  ],
+  [
+    "ø",
+    {
+      il: "mb5m7",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "o",
+    {
+      il: "mb5M6",
+      prune: [],
+      ren: { ...ren_all, M6: "♭♭7" },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "m6",
+    {
+      il: "m3m6",
+      prune: [],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7(9)",
+    {
+      il: "Mm7m9",
+      prune: ["P5"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7(♭9)",
+    {
+      il: "Mm7b9",
+      prune: ["P5"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7(#9)",
+    {
+      il: "Mm7#9",
+      prune: ["P5"],
+      ren: { ...ren_all, A9: "#9" },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7(♭13)",
+    {
+      il: "Mb13",
+      prune: ["M9", "P11", "P5"],
+      ren: { ...ren_all },
+      alt: ["7(#5)"],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "7(13)",
+    {
+      il: "Mm7m13",
+      prune: ["M9", "P11", "P5"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "m7(9)",
+    {
+      il: "mm7m9",
+      prune: ["P5"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "m7(11)",
+    {
+      il: "mm7m11",
+      prune: ["P5", "M9"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
+  [
+    "Δ(13)",
+    {
+      il: "MM7m13",
+      prune: ["P5", "P11", "M9"],
+      ren: { ...ren_all },
+      alt: [],
+      caged: { ...caged_all },
+    },
+  ],
 ]);
